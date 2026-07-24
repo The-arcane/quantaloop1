5d30aba4dfc19eb3b239ae76c6b325a078947fef
The-arcane <raunaq.adlakha@gmail.com>
2026-07-24 23:16:59 +0530

    Add launchpad redirect

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/launchpad',
+        destination:
+          'https://docs.google.com/forms/d/e/1FAIpQLScTK9u11PJwpTUl1PTlJMQVjsoYhGbS0XYUvoBV8Z_B6-zFcg/viewform',
+        permanent: true,
+      },
+    ];
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
